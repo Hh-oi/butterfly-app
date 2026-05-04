843435b7aa0e00d61fb69fd074cf564b9b5d6582
Hh-oi <lei.vill.20@gmail.com>
2026-05-04 08:31:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,30 @@
-const CACHE_NAME = 'butterfly-cache-v1';
-const urlsToCache = ['./', './index.html', './Logo.jpeg', './manifest.json'];
+const CACHE_NAME = 'butterfly-cache-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './Logo.jpeg',
+  './manifest.json'
+];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request).catch(() => caches.match('./index.html')))
   );
 });
